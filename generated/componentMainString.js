7b94c60b15ae0d30439c98a2188b628acb31daf6
k23Burke <k23burke@gmail.com>
2016-04-17 23:37:43 -0400

    generation in full effect

--- generated/componentMainString.js
+++ generated/componentMainString.js
@@ -1,13 +1,11 @@
-export default = "import React from 'react';"+'\n'+
+module.exports = function(name, propString) {
+	return "import React from 'react';"+'\n'+
 "import PureRenderMixin from 'react-addons-pure-render-mixin';"+'\n'+
 "import { connect } from 'react-redux';"+'\n'+
 "import { fromJS } from 'immutable';"+'\n'+
-"import * as _ from  'lodash';"+'\n'+
-""+'\n'+
-"import { API_BASE_ENDPOINT } from '../../constants/endPoints';"+'\n'+
 ""+'\n'+
 ""+'\n'+
-"export default class NewComponent extends React.Component {"+'\n'+
+"export default class " + name + " extends React.Component {"+'\n'+
 "  constructor(props) {"+'\n'+
 "    //defaults here"+'\n'+
 "    props = Object.assign({"+'\n'+
@@ -21,21 +19,6 @@ export default = "import React from 'react';"+'\n'+
 "  //component lifecycle methods"+'\n'+
 "  componentDidMount() {"+'\n'+
 "    console.log('this.props at Mount:', this.props)"+'\n'+
-"  }"+'\n'+
-""+'\n'+
-"  renderOption(item) {"+'\n'+
-"    if (!item.airports){ return <div className='simple-option'>Seach for a US Airport</div>};"+'\n'+
-"    return ("+'\n'+
-"      <div className='simple-option' style={{fontSize: 12}}>"+'\n'+
-"        <div>{item.city}, {item.region}</div>"+'\n'+
-"        <div>{item.name} ({item.code})</div>"+'\n'+
-"        <ul>"+'\n'+
-"          {item.airports.map( (airport, i) =>"+'\n'+
-"            <li key={i}>{airport.name}<small>({airport.iata_code})</small></li>"+'\n'+
-"          )}"+'\n'+
-"        </ul>"+'\n'+
-"      </div>"+'\n'+
-"    )"+'\n'+
 "  }"+'\n'+
 ""+'\n'+
 "  render() {"+'\n'+
@@ -57,20 +40,14 @@ export default = "import React from 'react';"+'\n'+
 "}"+'\n'+
 ""+'\n'+
 ""+'\n'+
-"export default connect("+'\n'+
-"  mapStateToProps,"+'\n'+
-"  { requestAirport,"+'\n'+
-"    addAirportToList,"+'\n'+
-"    updateTripSearch }"+'\n'+
-")(TripSearch);"+'\n'+
-""+'\n'+
 "export default connect({"+'\n'+
 "  mapStateToProps,"+'\n'+
 "  {"+'\n'+
 "    //action creators"+'\n'+
 "  }"+'\n'+
-"})(NewComponent)"+'\n'+
+"})(" + name + ")"+'\n'+
 ""+'\n'+
-"NewComponent.propTypes = {"+'\n'+
-"  // props: React.PropTypes.object,"+'\n'+
-"};"+'\n'
+name + ".propTypes = {"+'\n'+
+propString + '\n' +
+"};"+'\n';
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
+var fs = require('fs');
 var ncp = require('ncp').ncp;
 var path = require('path');
+var compGenerator = require('./generated/componentMainString');
 
 // file paths
 var currentLocation = process.cwd() + '/' + process.argv[2] + '.js';
@@ -8,15 +10,22 @@ var componentFilePath = path.join(__dirname + '/generated/componentMain.js');
 
 
 // create new files
-ncp.limit = 16;
-ncp(componentFilePath, currentLocation, function (err) {
-	if (err) {
-		return console.error(err);
-	}
-	console.log('done!');
-});
+// ncp.limit = 16;
+// ncp(componentFilePath, currentLocation, function (err) {
+// 	if (err) {
+// 		return console.error(err);
+// 	}
+// 	console.log('done!');
+// });
 
-displayOptions(process.argv.slice(3));
+fs.writeFile(currentLocation, compGenerator(process.argv[2], displayOptions(process.argv.slice(3))), function(err){
+	console.log('error: ', err)
+})
+
+// console.log('string: \n', compGenerator(process.argv[2], displayOptions(process.argv.slice(3))))
+// fs.createWriteStream()
+
+// displayOptions(process.argv.slice(3));
 
 
 
@@ -26,19 +35,19 @@ function displayOptions(list) {
 	var spacing = '    ';
 	var propTypeString = '';
 
-
 	list.map(function(each) {
 		var keyValue = each.split(':');
 		if (keyValue[1] === 'date' || keyValue[1] === 'moment' ||
 				keyValue[1] === 'date!' || keyValue[1] === 'moment!') {
-			propTypeString += spacing + keyValue[0] + ': React.PropType.instanceOf(';
+			propTypeString += spacing + keyValue[0] + ': React.PropTypes.instanceOf(';
 			propTypeString += isRequired(keyValue[1]) ? keyValue[1].substr(0,keyValue[1].length - 1) + ').isRequired,\n' : keyValue[1] + '),\n';
 		} else {
-			propTypeString += spacing + keyValue[0] + ': React.PropType.';
+			propTypeString += spacing + keyValue[0] + ': React.PropTypes.';
 			propTypeString += isRequired(keyValue[1]) ? keyValue[1].substr(0,keyValue[1].length - 1) + '.isRequired,\n' : keyValue[1] + ',\n';
 		}
 	})
-	console.log('\n\n\n\npropTypeString', propTypeString)
+	return propTypeString;
+	// console.log('\n\n\n\npropTypeString', propTypeString)
 
 
 	function isRequired(string) {
